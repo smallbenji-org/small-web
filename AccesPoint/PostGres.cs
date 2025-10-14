13aa8964b31b4656be19f9ba7d2a63a45edd72e0
emilh <61194@edu.sde.dk>
2025-10-14 10:57:19 +0200

    AccesPoint til Npgsql

---
+++ AccesPoint/PostGres.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+using Dapper;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace AccesPoint
+{
+    public class PostGres : IDbHandler
+    {
+        private readonly IConfiguration configuration;
+
+        public PostGres(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IDataReader ExecuteReader(string query)
+        {
+            using NpgsqlConnection conn = new NpgsqlConnection(configuration.GetConnectionString("defaultConnection"));
+            return conn.ExecuteReader(query);
+        }
+
+        public object ExecuteScalar(string query, object param = null)
+        {
+            using NpgsqlConnection conn = new NpgsqlConnection(configuration.GetConnectionString("defaultConnection"));
+            return conn.ExecuteScalar(query, param);
+        }
+
+        public object Execute(string query, object param = null)
+        {
+            using NpgsqlConnection conn = new NpgsqlConnection(configuration.GetConnectionString("defaultConnection"));
+            return conn.Execute(query, param);
+        }
+    }
+}
