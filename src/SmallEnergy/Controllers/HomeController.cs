64b82ca3c4c21b5040d230548664cb2f42bbf356
emilh <61194@edu.sde.dk>
2025-10-21 10:42:50 +0200

    Made base for routing the authentication. Working on the actual authentication now

--- src/SmallEnergy/Controllers/HomeController.cs
+++ src/SmallEnergy/Controllers/HomeController.cs
@@ -32,11 +32,6 @@ namespace SmallEnergy.Controllers
             return View();
         }
 
-        public IActionResult Login()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
